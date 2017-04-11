20d74e568d24bf254a241ad6ffbe1b3ef8409356
Robert Brice <rob.a.brice@gmail.comC>
2017-04-11 21:36:19 +0100

    add some css to button

--- public/MapWrapper.js
+++ public/MapWrapper.js
@@ -5,13 +5,11 @@ var MapWrapper = function(container, coords, zoom){
     zoom: zoom
   });
 
-
   var controlDiv = document.createElement('div');
   var centerControl = new CenterControl(controlDiv, this.googleMap);
 
   controlDiv.index = 1;
   this.googleMap.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(controlDiv);
-
 
 }
 
@@ -20,41 +18,43 @@ var MapWrapper = function(container, coords, zoom){
 function CenterControl(controlDiv, map) {
 
   var edinburgh = {lat: 55.9533, lng: -3.1883};
+  //var tamarama  
 
   // Set CSS for the control border.
   var controlUI = document.createElement('div');
   controlUI.style.backgroundColor = '#fff';
   controlUI.style.border = '2px solid #fff';
-  controlUI.style.borderRadius = '3px';
+  controlUI.style.borderRadius = '2px';
   controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
   controlUI.style.cursor = 'pointer';
-  controlUI.style.marginBottom = '22px';
+  controlUI.style.marginBottom = '18px';
   controlUI.style.textAlign = 'center';
-  controlUI.title = 'Click to go tot Edinburgh';
+  controlUI.title = 'Click to go to Edinburgh';
   controlDiv.appendChild(controlUI);
 
   // Set CSS for the control interior.
   var controlText = document.createElement('div');
   controlText.style.color = 'rgb(25,25,25)';
   controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
-  controlText.style.fontSize = '16px';
-  controlText.style.lineHeight = '38px';
+  controlText.style.fontSize = '12px';
+  controlText.style.lineHeight = '20px';
   controlText.style.paddingLeft = '5px';
   controlText.style.paddingRight = '5px';
   controlText.innerHTML = 'Go To Edinburgh';
   controlUI.appendChild(controlText);
 
   // Setup the click event listeners: simply set the map to Chicago.
+
+
   controlUI.addEventListener('click', function() {
+  
     map.setCenter(edinburgh);
+    //controlUI.title = 'Click to go to Sydney';
+    //controlText.innerHTML = 'Go to Sydney';
     map.controls[google.maps.ControlPosition.BOTTOM_CENTER].clear();
   });
 
 }
-
-
-
-
 
 
 MapWrapper.prototype = {
