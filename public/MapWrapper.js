226e446c5e7266ada8005b7a539e7aba1b9eef6b
Robert Brice <rob.a.brice@gmail.comC>
2017-04-11 19:13:40 +0100

    added button to bottom of map

--- public/MapWrapper.js
+++ public/MapWrapper.js
@@ -5,7 +5,57 @@ var MapWrapper = function(container, coords, zoom){
     zoom: zoom
   });
 
+
+  var controlDiv = document.createElement('div');
+  var centerControl = new CenterControl(controlDiv, this.googleMap);
+
+  controlDiv.index = 1;
+  this.googleMap.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(controlDiv);
+
+
 }
+
+
+
+function CenterControl(controlDiv, map) {
+
+  var edinburgh = {lat: 55.9533, lng: -3.1883};
+
+  // Set CSS for the control border.
+  var controlUI = document.createElement('div');
+  controlUI.style.backgroundColor = '#fff';
+  controlUI.style.border = '2px solid #fff';
+  controlUI.style.borderRadius = '3px';
+  controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+  controlUI.style.cursor = 'pointer';
+  controlUI.style.marginBottom = '22px';
+  controlUI.style.textAlign = 'center';
+  controlUI.title = 'Click to go tot Edinburgh';
+  controlDiv.appendChild(controlUI);
+
+  // Set CSS for the control interior.
+  var controlText = document.createElement('div');
+  controlText.style.color = 'rgb(25,25,25)';
+  controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+  controlText.style.fontSize = '16px';
+  controlText.style.lineHeight = '38px';
+  controlText.style.paddingLeft = '5px';
+  controlText.style.paddingRight = '5px';
+  controlText.innerHTML = 'Go To Edinburgh';
+  controlUI.appendChild(controlText);
+
+  // Setup the click event listeners: simply set the map to Chicago.
+  controlUI.addEventListener('click', function() {
+    map.setCenter(edinburgh);
+    map.controls[google.maps.ControlPosition.BOTTOM_CENTER].clear();
+  });
+
+}
+
+
+
+
+
 
 MapWrapper.prototype = {
 
@@ -24,6 +74,9 @@ MapWrapper.prototype = {
           infoWindow.open(this.googleMap, marker);
         });
   },
+
+
+
 
   addClickEvent: function() {
   
--- public/app.js
+++ public/app.js
@@ -1,22 +1,22 @@
 var initialise = function(){
 
- var LOCATIONS = [
+ var LOCATIONS_ARRAY = [
       {pos: {lat: -33.9004, lng: 151.2704}, info: "<p>This is Tamarama, a great beach but you've got to watch the surf - there's a dangerous rip when its rough!</p>"},
-      {pos: {lat: -33.7653, lng: 151.2704}, info: "<p>This is where BP Solar Australia used to be</p>"}
+      {pos: {lat: -33.8568, lng: 151.2153}, info: "<p>Sydney Opera House!</p>"},
+      {pos: {lat: -33.8915, lng: 151.2767}, info: "<p>Bondi Beach!</p>"},
+      {pos: {lat: -33.7653, lng: 151.2704}, info: "<p>BP Solar Australia used to be here</p>"},
+      {pos: {lat: 55.9533, lng: -3.1883}, info: "<p>Welome to Edinburgh</p>"}
       ]
 
   var container = document.getElementById('main-map');
-  var mainMap = new MapWrapper(container, LOCATIONS[0]['pos'], 10);
+  var mainMap = new MapWrapper(container, LOCATIONS_ARRAY[0]['pos'], 10);
 
-  LOCATIONS.forEach(function(location){
+  LOCATIONS_ARRAY.forEach(function(location){
     mainMap.addMarker(location['pos'], location['info']);    
   })
 
-//  mainMap.addClickEvent();
-
-
-
 }
+
 
 
 window.onload = initialise;
